547fddcf80e7786416fe0690503bcf97dfe21780
Lihan Li <lilihan.it@gmail.com>
2015-11-12 13:53:11 +1100

    Fixing the latex output for nested inline editable does not have "\editable {}"

--- src/commands/math/commands.js
+++ src/commands/math/commands.js
@@ -822,4 +822,6 @@ LatexCmds.MathQuillMathField = P(MathCommand, function(_, super_) {
 // Add old editable support
 LatexCmds.editable = P(LatexCmds.MathQuillMathField, function(_, super_) {
   _.ctrlSeq = '\\editable';
+  // Fix the latex output so it contains "\editable {}"
+  _.latex = function(){ return '\\editable{' + this.ends[L].latex() + '}'};
 });
