12557f4d8642fd650eee2923892d39ed0646d4ab
Lihan Li <lilihan.it@gmail.com>
2015-12-02 15:52:14 +1100

    Fixing typing BackSpace in box mode will erase the contents outside the box

--- test/unit/mathspace.test.js
+++ test/unit/mathspace.test.js
@@ -23,3 +23,25 @@ suite('Mathspace Features', function() {
         assert.equal(rootBlock.jQ.children('.thousands-separator-after').length, 0);
     });
 });
+
+
+suite('Mathspace Features: Nested inline editable', function() {
+    var mq;
+    var rootBlock;
+    var controller;
+    setup(function() {
+        mq = MathQuill.MathField($('<span></span>').appendTo('#mock')[0]);
+        rootBlock = mq.__controller.root;
+        controller = mq.__controller;
+    });
+    teardown(function() {
+        $(mq.el()).remove();
+    });
+
+    test('delete nested editable box contents should not modify outside the box', function() {
+        mq.latex('\\editable{1}+2');
+        rootBlock.jQ.find('.mq-inner-editable').trigger('mousedown');
+        mq.keystroke('Backspace');
+        assert.equal(mq.latex(), '\\editable{}+2');
+    });
+});
