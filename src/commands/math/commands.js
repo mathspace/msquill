8d8efdbd1f64993b379abcfd2e3a309bea8de844
Andrew Hyndman <ajhyndman@hotmail.com>
2016-02-09 11:22:03 +1100

    Refactor - extract nCr/nPr into mathspace custom commands

--- src/commands/math/commands.js
+++ src/commands/math/commands.js
@@ -484,63 +484,6 @@ LatexCmds.nthroot = P(SquareRoot, function(_, super_) {
 });
 
 
-
-/* MaThSpACe hacK begin */
-/* Add custom commands  */
-
-
-var nCr = LatexCmds.nCr = P(MathCommand, function(_, super_) {
-  _.ctrlSeq = '\\nCr';
-  _.htmlTemplate =
-      '<span>'
-          + '<span class="mq-supsub mq-sup-only mq-non-leaf">'
-          +     '<span class="mq-sup">'
-          +         '<span>&0</span>'
-          +     '</span>'
-          + '</span>'
-          + '<var>C</var>'
-          + '<span class="mq-supsub mq-non-leaf">'
-          +     '<span class="mq-sub">'
-          +         '<span>&1</span>'
-          +     '</span>'
-          + '</span>'
-    + '</span>'
-  ;
-  _.text_template = ['nCr[', '](', ')'];
-  _.latex = function() {
-    return '\\nCr{'+this.ends[L].latex()+'}{'+this.ends[R].latex()+'}';
-  };
-});
-
-
-
-var nPr = LatexCmds.nPr = P(MathCommand, function(_, super_) {
-  _.ctrlSeq = '\\nPr';
-  _.htmlTemplate =
-      '<span>'
-          + '<span class="mq-supsub mq-sup-only mq-non-leaf">'
-          +     '<span class="mq-sup">'
-          +         '<span>&0</span>'
-          +     '</span>'
-          + '</span>'
-          + '<var>P</var>'
-          + '<span class="mq-supsub mq-non-leaf">'
-          +     '<span class="mq-sub">'
-          +         '<span>&1</span>'
-          +     '</span>'
-          + '</span>'
-    + '</span>'
-  ;
-  _.text_template = ['nPr[', '](', ')'];
-  _.latex = function() {
-    return '\\nPr{'+this.ends[L].latex()+'}{'+this.ends[R].latex()+'}';
-  };
-});
-
-
-/* MaThSpACe hacK end */
-
-
 function DelimsMixin(_, super_) {
   _.jQadd = function() {
     super_.jQadd.apply(this, arguments);
---
+++ src/commands/mathspace/mathspaceCommands.js
@@ -0,0 +1,50 @@
+/* Add custom commands  */
+
+
+var nCr = LatexCmds.nCr = P(MathCommand, function(_, super_) {
+  _.ctrlSeq = '\\nCr';
+  _.htmlTemplate =
+      '<span>'
+          + '<span class="mq-supsub mq-sup-only mq-non-leaf">'
+          +     '<span class="mq-sup">'
+          +         '<span>&0</span>'
+          +     '</span>'
+          + '</span>'
+          + '<var>C</var>'
+          + '<span class="mq-supsub mq-non-leaf">'
+          +     '<span class="mq-sub">'
+          +         '<span>&1</span>'
+          +     '</span>'
+          + '</span>'
+    + '</span>'
+  ;
+  _.text_template = ['nCr[', '](', ')'];
+  _.latex = function() {
+    return '\\nCr{'+this.ends[L].latex()+'}{'+this.ends[R].latex()+'}';
+  };
+});
+
+
+
+var nPr = LatexCmds.nPr = P(MathCommand, function(_, super_) {
+  _.ctrlSeq = '\\nPr';
+  _.htmlTemplate =
+      '<span>'
+          + '<span class="mq-supsub mq-sup-only mq-non-leaf">'
+          +     '<span class="mq-sup">'
+          +         '<span>&0</span>'
+          +     '</span>'
+          + '</span>'
+          + '<var>P</var>'
+          + '<span class="mq-supsub mq-non-leaf">'
+          +     '<span class="mq-sub">'
+          +         '<span>&1</span>'
+          +     '</span>'
+          + '</span>'
+    + '</span>'
+  ;
+  _.text_template = ['nPr[', '](', ')'];
+  _.latex = function() {
+    return '\\nPr{'+this.ends[L].latex()+'}{'+this.ends[R].latex()+'}';
+  };
+});
