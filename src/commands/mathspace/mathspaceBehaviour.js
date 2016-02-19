cab127470edb70040733af319a461866c837aa68
Lihan Li <lilihan.it@gmail.com>
2016-02-19 13:12:52 +0800

    Added limit custom command

--- src/commands/mathspace/mathspaceBehaviour.js
+++ src/commands/mathspace/mathspaceBehaviour.js
@@ -16,3 +16,27 @@ LatexCmds.comma = LatexCmds[','] = P(Symbol, function(_, super_) {
     );
   };
 });
+
+// limit symbol
+LatexCmds.lim = P(MathCommand, function(_, super_) {
+  _.ctrlSeq = '\\lim';
+  _.init = function() {
+    var htmlTemplate =
+      '<span class="mq-lim mq-non-leaf">'
+    +   '<span class="mq-un-italicized">lim</span>'
+    +   '<span class="mq-approaches">'
+    +     '<span>'
+    +        '<span>&0</span>'
+    +        '<span class="mq-binary-operator">→</span>'
+    +        '<span>&1</span>'
+    +      '</span>'
+    +    '</span>'
+    + '</span>'
+    ;
+    Symbol.prototype.init.call(this, '\\lim ', htmlTemplate);
+  };
+  _.latex = function() {
+    var b = this.blocks;
+    return this.ctrlSeq + '{' + b[0].latex() +'}{' + b[1].latex() + '}';
+  };
+});
