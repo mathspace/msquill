943ff617208d8b61d2074fdc26c1913e0646a1fb
Lihan Li <lilihan.it@gmail.com>
2016-02-20 13:09:04 +0800

    Added \int support (Our version of \int has placeholders)

--- src/commands/mathspace/mathspaceBehaviour.js
+++ src/commands/mathspace/mathspaceBehaviour.js
@@ -17,3 +17,50 @@ LatexCmds.comma = LatexCmds[','] = P(Symbol, function(_, super_) {
   };
 });
 
+
+// limit symbol
+LatexCmds.lim = P(MathCommand, function(_, super_) {
+  _.ctrlSeq = '\\lim';
+  _.init = function() {
+    var htmlTemplate =
+      '<span class="mq-lim mq-non-leaf">'
+    +   '<span class="mq-un-italicized">lim</span>'
+    +   '<span class="mq-approaches">'
+    +     '<span>'
+    +        '<span>&0</span>'
+    +        '<span class="mq-binary-operator">→</span>'
+    +        '<span>&1</span>'
+    +      '</span>'
+    +    '</span>'
+    + '</span>'
+    ;
+    Symbol.prototype.init.call(this, '\\lim ', htmlTemplate);
+  };
+  _.latex = function() {
+    var b = this.blocks;
+    return this.ctrlSeq + '{' + b[0].latex() +'}{' + b[1].latex() + '}';
+  };
+});
+
+
+LatexCmds['∫'] =
+LatexCmds['int'] =
+LatexCmds.integral = P(MathCommand, function(_, super_) {
+  _.ctrlSeq = '\\int';
+  _.init = function() {
+    var htmlTemplate =
+      '<span>' +
+        '<big>∫</big>' +
+        '<span class="mq-supsub mq-non-leaf mq-int">' +
+          '<span class="mq-sup">&0</span>' +
+          '<span class="mq-sub">&1</span>' +
+        '</span>' +
+      '</span>'
+    ;
+    Symbol.prototype.init.call(this, '\\int ', htmlTemplate);
+  };
+  _.latex = function() {
+    var b = this.blocks;
+    return this.ctrlSeq + '{' + b[0].latex() +'}{' + b[1].latex() + '}';
+  };
+});
